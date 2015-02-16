4672589e176d935cd7481b7d1edccafffbba01ae
Angel Ruiz <aruizca@gmail.com>
2015-02-16 11:46:49 +1100

    Ignores ClientAbortExceptions when logging the error out.

--- ajaxanywhere/src/main/java/com/nerderg/ajaxanywhere/AAFilter.java
+++ ajaxanywhere/src/main/java/com/nerderg/ajaxanywhere/AAFilter.java
@@ -75,8 +75,11 @@ public class AAFilter implements Filter {
                     XMLHandler.sendRedirect(bufferResponseWrapper);
                 }
             } catch (Throwable e) {
-                logger.log(Level.SEVERE,e.getMessage(), e);
-                XMLHandler.handleError(response, e);
+                String simpleName = e.getCause().getClass().getSimpleName();
+                if (!simpleName.equals("ClientAbortException")) {
+                    logger.log(Level.SEVERE, e.getMessage(), e);
+                    XMLHandler.handleError(response, e);
+                }
             }
             logger.info("AjaxAnywhere Request: \"" + requestMapping + "\" took: " + (System.currentTimeMillis() - start) + "ms" );
         }
